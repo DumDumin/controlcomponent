f649e35dfabb44131e6860345093c2000877a55f
Tobias <tobias0605@googlemail.com>
2021-03-24 17:08:48 +0100

    First ExecutionStates

---
+++ src/ControlComponent/ControlComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ControlComponent
+{
+    public class ControlComponent
+    {
+        private ExecutionState exst = ExecutionState.STOPPED;
+        public ExecutionState EXST => exst;
+
+        public void Reset()
+        {
+            exst = ExecutionState.IDLE;
+        }
+
+        public void Start()
+        {
+            if(exst == ExecutionState.IDLE)
+            {
+                exst = ExecutionState.EXECUTE;
+            }
+        }
+    }
+}
---
+++ tests/ControlComponent.Tests/ControlComponentTests.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+
+namespace ControlComponent.Tests
+{
+    public class ControlComponentTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Given_NewControlComponent_When_EXST_Then_Stopped()
+        {
+            ControlComponent cc = new ControlComponent();
+            Assert.AreEqual(ExecutionState.STOPPED, cc.EXST);
+        }
+
+        [Test]
+        public void Given_StoppedControlComponent_When_Reset_Then_Idle()
+        {
+            ControlComponent cc = new ControlComponent();
+            cc.Reset();
+            Assert.AreEqual(ExecutionState.IDLE, cc.EXST);
+        }
+
+        [Test]
+        public void Given_StoppedControlComponent_When_Start_Then_Stopped()
+        {
+            ControlComponent cc = new ControlComponent();
+            cc.Start();
+            Assert.AreEqual(ExecutionState.STOPPED, cc.EXST);
+        }
+
+        [Test]
+        public void Given_IdleControlComponent_When_Start_Then_Execute()
+        {
+            ControlComponent cc = new ControlComponent();
+            cc.Reset();
+            cc.Start();
+            Assert.AreEqual(ExecutionState.EXECUTE, cc.EXST);
+        }
+    }
+}
