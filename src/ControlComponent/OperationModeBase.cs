8c8ef385901324768ff50202bf1334259cd3d4a0
Tobias <tobias0605@googlemail.com>
2021-03-30 16:59:48 +0200

    Made OperationMode to an abstract class

---
+++ src/ControlComponent/OperationModeBase.cs
@@ -0,0 +1,311 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ControlComponent
+{
+    public abstract class OperationModeBase
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        Dictionary<ExecutionState, Func<CancellationToken, Task>> stateActions;
+        public string OpModeName { get; }
+        IExecution execution;
+
+        CancellationTokenSource executionTokenSource;
+        CancellationTokenSource mainTokenSource;
+
+        IDictionary<string, OrderOutput> outputs;
+        public ReadOnlyCollection<string> neededRoles;
+
+        public OperationModeBase(string name) : this(name, new Collection<string>())
+        {
+        }
+
+        public OperationModeBase(string name, Collection<string> neededRoles)
+        {
+            OpModeName = name;
+            this.neededRoles = new ReadOnlyCollection<string>(neededRoles);
+
+            stateActions = new Dictionary<ExecutionState, Func<CancellationToken, Task>>()
+            {
+                { ExecutionState.RESETTING  , Resetting },
+                { ExecutionState.IDLE       , Idle },
+                { ExecutionState.STARTING   , Starting },
+                { ExecutionState.EXECUTE    , Execute },
+                { ExecutionState.COMPLETING , Completing },
+                { ExecutionState.COMPLETED  , Completed },
+
+                { ExecutionState.HOLDING    , Holding },
+                { ExecutionState.HELD       , Held },
+                { ExecutionState.UNHOLDING  , Unholding },
+
+                { ExecutionState.SUSPENDING    , Suspending },
+                { ExecutionState.SUSPENDED     , Suspended },
+                { ExecutionState.UNSUSPENDING  , Unsuspending },
+
+                { ExecutionState.STOPPING       , Stopping},
+                { ExecutionState.STOPPED       , Stopped},
+
+                { ExecutionState.ABORTED        , Aborted },
+                { ExecutionState.ABORTING       , Aborting },
+
+                { ExecutionState.CLEARING        , Clearing },
+            };
+        }
+
+        private void OnExecutionStateChanged(object sender, EventArgs e)
+        {
+            executionTokenSource.Cancel();
+        }
+
+        public void Deselect()
+        {
+            // The order is important to avoid a new execution task to begin before the main loop is canceled
+            mainTokenSource.Cancel();
+            executionTokenSource.Cancel();
+        }
+
+        private Dictionary<string, Task> runningOutputs = new Dictionary<string, Task>();
+
+        protected void SelectRole(string role, string operationMode)
+        {
+            runningOutputs.Add(role, outputs[role].SelectOperationMode(operationMode));
+        }
+
+        protected abstract void Selected();
+
+        public async Task Select(IExecution execution, IDictionary<string, OrderOutput> orderOutputs)
+        {
+            try
+            { 
+                mainTokenSource = new CancellationTokenSource();
+                this.execution = execution;
+                this.outputs = orderOutputs;
+
+                foreach (var item in neededRoles)
+                {
+                    if(!orderOutputs.Keys.Contains(item))
+                    {
+                        throw new Exception($"Role {item} is not available in known outputs = {string.Join(' ', orderOutputs.Keys)}");
+                    }
+                }
+
+                Selected();
+
+                execution.ExecutionStateChanged += OnExecutionStateChanged;
+
+                while(!mainTokenSource.IsCancellationRequested)
+                {
+                    executionTokenSource = new CancellationTokenSource();
+                    logger.Debug($"Invoke {execution.EXST} action");
+                    await stateActions[execution.EXST].Invoke(executionTokenSource.Token);
+                }
+                execution.ExecutionStateChanged -= OnExecutionStateChanged;
+            }
+            catch (System.Exception e)
+            {
+                logger.Error(e);
+                throw e;
+            }
+            finally
+            {
+                foreach (var output in runningOutputs)
+                {
+                    await outputs[output.Key].DeselectOperationMode();
+                }
+            }
+        }
+
+        private async Task WaitForOutputsToDo(Action<OrderOutput> action, ExecutionState state)
+        {
+            List<Task> waitForOutputStates = new List<Task>();
+            foreach (var output in outputs.Values.Where(o => o.OpModeName != "NONE"))
+            {
+                // TODO might need the occupier if the cc of this opmode
+                action(output);
+                waitForOutputStates.Add(Helper.WaitForState(output, state));
+            }
+            await Task.WhenAll(waitForOutputStates);
+
+            execution.SetState(state);
+            await Task.CompletedTask;
+        }
+
+        protected virtual async Task Resetting(CancellationToken token)
+        {
+            await WaitForOutputsToDo((OrderOutput output) => output.Reset(this.OpModeName) , ExecutionState.IDLE);
+        }
+
+        protected virtual async Task Starting(CancellationToken token)
+        {
+            execution.SetState(ExecutionState.EXECUTE);
+            await Task.CompletedTask;
+        }
+
+        protected virtual async Task Stopping(CancellationToken token)
+        {
+            await WaitForOutputsToDo((OrderOutput output) => output.Stop(this.OpModeName) , ExecutionState.STOPPED);
+            // if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //     output.Value.Cc.Stop(control.ComponentName);
+        }
+
+        // Reset has to set EXST to IDLE after completion.
+        // public virtual Task Reset()
+        // {
+        //     foreach (var output in control.OrderOutputs)
+        //     {
+        //         if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+        //         // TOBI TODO how to wait for sub control components to reach idle ?? -> Update loop in MonoBehaviour
+        //             output.Value.Cc.Reset(control.ComponentName);
+        //     }
+        //     _WORKST = "BSTATE";
+        //     return Task.FromResult(ExecutionState.IDLE);
+        // }
+        // Abort has to set EXST to ABORTED after completion.
+
+        protected virtual async Task Aborting(CancellationToken token)
+        {
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Abort(control.ComponentName);
+            // }
+            // await Task.Run( async () => {
+            //     control.OrderOutputs.Values.All((OrderOutput o) => o.Cc.EXST == ExecutionState.ABORTED);
+            //     await Task.Delay(25);
+            // });
+
+            execution.SetState(ExecutionState.ABORTED);
+            await Task.CompletedTask;
+        }
+
+        // Clear has to set EXST to STOPPED after completion.
+        protected virtual async Task Clearing(CancellationToken token)
+        {
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Clear(control.ComponentName);
+            // }
+
+            // Task clearing = Clearing(token);
+
+            // // Wait for OrderOutputs to be stopped
+            // await Task.Run( async () => {
+            //     control.OrderOutputs.Values.All((OrderOutput o) => o.Cc.EXST == ExecutionState.STOPPED);
+            //     await Task.Delay(25);
+            // });
+
+            // await clearing;
+
+            execution.SetState(ExecutionState.STOPPED);
+            await Task.CompletedTask;
+        }
+        // Hold has to set EXST to HELD after completion.
+        protected virtual async Task Holding(CancellationToken token)
+        {
+            // TOBI TODO stay in this state till all outputs are done with hold
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Hold(control.ComponentName);
+            // }
+            execution.SetState(ExecutionState.HELD);
+            await Task.CompletedTask;
+        }
+        // Unhold has to set EXST to EXECUTE after completion.
+        protected virtual async Task Unholding(CancellationToken token)
+        {
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Unhold(control.ComponentName);
+            // }
+            execution.SetState(ExecutionState.EXECUTE);
+            await Task.CompletedTask;
+        }
+
+        protected virtual async Task Suspending(CancellationToken token)
+        {
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Suspend(control.ComponentName);
+            // }
+
+            // Task suspending = Suspending(token);
+
+            // await Task.Run( async () => {
+            //     control.OrderOutputs.Values.All((OrderOutput o) => o.Cc.EXST == ExecutionState.STOPPED);
+            //     await Task.Delay(25);
+            // });
+
+            // await suspending;
+
+            execution.SetState(ExecutionState.SUSPENDED);
+            await Task.CompletedTask;
+        }
+
+        // Unhold has to set EXST to EXECUTE after completion.
+        protected virtual async Task Unsuspending(CancellationToken token)
+        {
+            // foreach (var output in control.OrderOutputs)
+            // {
+            //     if (output.Value.Cc != null && output.Value.Cc.IsOccupied(control.ComponentName))
+            //         output.Value.Cc.Unsuspend(control.ComponentName);
+            // }
+            execution.SetState(ExecutionState.EXECUTE);
+            await Task.CompletedTask;
+        }
+
+        /*
+        * OnExecute is called cyclic, if OnSelected isn't overriden.
+        * If execution is finished set EXST to COMPLETING an call OnCompleting():
+        * <code>
+        * control.EXST = ExecutionState.COMPLETING;
+        * OnCompleting();
+        * </code>
+        * //TODO maybe its better to use this as a coroutine ?
+        */
+        protected virtual async Task Execute(CancellationToken token)
+        {
+            execution.SetState(ExecutionState.COMPLETING);
+            await Task.CompletedTask;
+        }
+
+        protected virtual async Task Completing(CancellationToken token)
+        {
+            execution.SetState(ExecutionState.COMPLETED);
+            await Task.CompletedTask;
+        }
+
+        protected virtual async Task Held(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});
+        }
+        protected virtual async Task Idle(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});;
+        }
+        protected virtual async Task Completed(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});
+        }
+        protected virtual async Task Aborted(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});
+        }
+        protected virtual async Task Stopped(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});
+        }
+        protected virtual async Task Suspended(CancellationToken token)
+        {
+            await Task.Delay(Timeout.Infinite, token).ContinueWith(task => {});
+        }
+    }
+}
--- tests/ControlComponent.Tests/OperationModeCascade.cs
+++ tests/ControlComponent.Tests/OperationModeCascade.cs
@@ -15,79 +15,11 @@ internal class OperationModeCascade : OperationMode
         roleToOpMode.Add("ROLE_TWO", "OpModeTwo");
     }
 
-    private async Task WaitToFinish(CancellationToken token)
-    {
-        int counter = 0;
-        while(!token.IsCancellationRequested && counter < 10)
-        {
-            await Task.Delay(1);
-            counter++;
-        }
-    }
-
-    private async Task DoTask(Func<CancellationToken, Task> action, CancellationToken token)
-    {
-        await WaitToFinish(token);
-
-        // If Execution finished normally call base method
-        if(!token.IsCancellationRequested)
-        {
-            await action(token);
-        }
-    }
-
-    protected override Task Resetting(CancellationToken token)
+    protected override void Selected()
     {
         foreach (var roleKV in roleToOpMode)
         {
             base.SelectRole(roleKV.Key, roleKV.Value);
         }
-
-        return DoTask(base.Resetting, token);
-    }
-
-    protected override Task Starting(CancellationToken token)
-    {
-        return DoTask(base.Starting, token);
-    }
-
-    protected override Task Stopping(CancellationToken token)
-    {
-        return DoTask(base.Stopping, token);
-    }
-
-    protected override Task Execute(CancellationToken token)
-    {
-        return DoTask(base.Execute, token);
-    }
-
-    protected override Task Completing(CancellationToken token)
-    {
-        return DoTask(base.Completing, token);
-    }
-
-    protected override Task Suspending(CancellationToken token)
-    {
-        return DoTask(base.Suspending, token);
-    }
-
-    protected override Task Unsuspending(CancellationToken token)
-    {
-        return DoTask(base.Unsuspending, token);
-    }
-
-    protected override Task Holding(CancellationToken token)
-    {
-        return DoTask(base.Holding, token);
-    }
-
-    protected override Task Unholding(CancellationToken token)
-    {
-        return DoTask(base.Unholding, token);
-    }
-
-    protected override Task Aborting(CancellationToken token)
-    {
-        return DoTask(base.Aborting, token);
     }
 }
