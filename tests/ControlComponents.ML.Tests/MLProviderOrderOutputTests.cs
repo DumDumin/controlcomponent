b4b80ec2bd43628b37d8e0ff31003151c469b0cb
Tobias <tobias0605@googlemail.com>
2021-07-09 11:16:45 +0200

    mlprovider endepisode test

--- tests/ControlComponents.ML.Tests/MLProviderOrderOutputTests.cs
+++ tests/ControlComponents.ML.Tests/MLProviderOrderOutputTests.cs
@@ -62,5 +62,22 @@ namespace ControlComponents.ML.Tests
 
             Assert.ThrowsAsync(typeof(InvalidOperationException), () => output.EndEpisode(1));
         }
+
+        [Test]
+        public async Task Given__When_DecideAndEndEpisode_Then_CorrectStateFlow()
+        {
+            var properties = new MLProperties(1, 1);
+            MLControlComponent mlprovider = new MLControlComponent("Provider", properties);
+            mlprovider.AddOperationMode(new MLProviderOperationModeTest(mlprovider));
+            MLProviderOrderOutput output = new MLProviderOrderOutput("Provider", "Provider", provider.Object, mlprovider);
+
+            await output.Decide(new float[1], new float[1], 0);
+            Assert.AreEqual(ExecutionState.COMPLETED, output.EXST);
+            Assert.AreEqual("Inference", output.OpModeName);
+
+            await output.EndEpisode(0);
+            Assert.AreEqual(ExecutionState.STOPPED, output.EXST);
+            Assert.AreEqual("NONE", output.OpModeName);
+        }
     }
 }
